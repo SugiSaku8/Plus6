6a42a1d19f0e508a4f27bc619b1886433bba5c37
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-02-21 09:28:52 +0900

    Create model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+import gradio as gr
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
+model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
+
+def generate_response(input_text):
+    inputs = tokenizer.encode(input_text + tokenizer.eos_token, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[:, inputs.shape[-1]:][0], skip_special_tokens=True)
+    return response
+
+iface = gr.Interface(fn=generate_response, inputs="text", outputs="text")
+iface.launch()
