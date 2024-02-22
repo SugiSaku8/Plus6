a7201a6d9d3297b2ff48253b8dd6dc2cc191dffe
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-02-22 02:24:16 +0000

    Updata Model

--- model.py
+++ model.py
@@ -1,22 +1,14 @@
 import gradio as gr
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
-model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large")
+tokenizer = AutoTokenizer.from_pretrained("LargeWorldModel/LWM-Text-Chat-1M")
+model = AutoModelForCausalLM.from_pretrained("LargeWorldModel/LWM-Text-Chat-1M")
 
 def generate_response(input_text):
-    #  入力テキストをトークナイザーでエンコード
-    inputs = tokenizer.encode_plus(input_text, return_tensors="pt")
-    #  モデルに入力を渡し、答えを生成
-    outputs = model(**inputs)
-    # 答えのスコアと開始位置を取得
-    answer_scores = outputs.start_logits
-    answer_starts = torch.argmax(answer_scores)  # get the most likely beginning of answer with the argmax of the score
-    # 答えの終了位置を計算
-    answer_ends = answer_starts + torch.argmax(outputs.end_logits[answer_starts]) +  1  # get the most likely end of answer with the argmax of the score
-    # 答えをトークナイザーでデコード
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_starts:answer_ends]))
-    return answer
+    inputs = tokenizer.encode(input_text + tokenizer.eos_token, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[:, inputs.shape[-1]:][0], skip_special_tokens=True)
+    return response
 
 iface = gr.Interface(fn=generate_response, inputs="text", outputs="text")
 iface.launch(share=True)
